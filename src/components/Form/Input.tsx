608ac7f1b2167e2d48978b8bc1a4d0b49bd39263
unknown <fernandoalvesq@gmail.com>
2021-07-26 17:46:18 -0300

    criando a home page, e o component input do form

---
+++ src/components/Form/Input.tsx
@@ -0,0 +1,55 @@
+import { Input as ChakraInput, FormLabel, FormControl, InputProps as ChakraInputProps, InputGroup, Button, InputRightElement } from '@chakra-ui/react';
+import { useState } from 'react';
+
+interface InputProps extends ChakraInputProps {
+  name: string,
+  label?: string,
+  activeShow?: boolean,
+}
+
+export function Input({ name, label, activeShow, type, ...rest }: InputProps) {
+  const [show, setShow] = useState(false);
+  const handleClick = () => setShow(!show);
+
+  return (
+    <FormControl>
+      <FormLabel htmlFor={name}>{label}</FormLabel>
+      <InputGroup size="md">
+      <ChakraInput
+        name={name}
+        id={name}
+        focusBorderColor="pink.500"
+        bgColor="gray.900"
+        variant="filled"
+        type={activeShow && show ? 
+                show ? "text" : "password"
+                : type
+        }
+        _hover={{
+          bgColor: "gray.900"
+        }}
+        size="lg"
+        {...rest}
+      />
+      {
+        activeShow ?
+        <InputRightElement width="4.5rem">
+        <Button 
+        h="1.75rem" size="sm" onClick={handleClick} margin='1'
+        bottom='-3px'
+        colorScheme="pink"
+        focusBorderColor="pink.500"
+        _focus={{
+          borderColor: "pink.500"
+        }}
+        >
+          {show ? "Ocultar" : "Mostrar"}
+        </Button>
+      </InputRightElement>
+        :
+        ''
+      }
+       </InputGroup>
+    </FormControl>
+  )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,31 @@
+import { Flex, Button, Stack } from '@chakra-ui/react';
+import {Input} from '../components/Form/input'
 
 export default function Home() {
   return (
-    <h1>Dashgo</h1>
+    <Flex
+      w="100vw"
+      h="100vh"
+      align="center"
+      justify="center"
+    >
+
+      <Flex
+        as='form'
+        width='100%'
+        maxWidth={360}
+        bg="gray.800"
+        p="8"
+        borderRadius={8}
+        flexDir="column"
+      >
+        <Stack spacing="4">
+            <Input type="email" label="E-mail" name="email" />
+            <Input activeShow={true} type="password" label="Senha" name="password" />
+        </Stack>
+
+        <Button type="submit" mt="6" colorScheme="pink" size="lg">Entrar</Button>
+      </Flex>
+    </Flex>
   )
 }
