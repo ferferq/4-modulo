c12ceabfe00bb6c09475d209dded4d429312a229
unknown <fernandoalvesq@gmail.com>
2021-07-29 21:03:05 -0300

    configuracao do miragejs, axios e tambem criacao da logica das paginas

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,29 @@
 import { AppProps } from 'next/app'
 import { ChakraProvider } from "@chakra-ui/react";
+import { QueryClient, QueryClientProvider } from 'react-query'
+import { ReactQueryDevtools } from 'react-query/devtools'
 import { theme } from '../styles/theme';
 import { SidebarDrawerProvider } from '../contexts/SidebarDrawerContext'
+import { makeServer } from '../services/mirage';
+
+if (process.env.NODE_ENV !== 'production') {
+  makeServer();
+}
+
+const queryClient = new QueryClient()
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     //resetCSS=false - mantem as configuracaos do broswer.
+    <QueryClientProvider client={queryClient}>
     <ChakraProvider resetCSS theme={theme}>
       <SidebarDrawerProvider>
-        <Component {...pageProps} />
+          <Component {...pageProps} />
       </SidebarDrawerProvider>
     </ChakraProvider>
+
+    <ReactQueryDevtools />
+  </QueryClientProvider>
 
   )
 }
--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -7,15 +7,20 @@ import {
   Table, Thead, Tr, Th, Td, Checkbox, Tbody,
   Text,
   useBreakpointValue,
-  IconButton
+  IconButton,
+  Spinner
 } from "@chakra-ui/react";
 import { RiPencilLine, RiUserAddLine } from "react-icons/ri";
 import { Header } from '../../components/Header'
 import { Sidebar } from "../../components/Sidebar";
 import { Pagination } from "../../components/Pagination"
 import Link from "next/link";
+import { useUsers } from "../../services/hooks/useUsers";
 
 export default function UserList() {
+  //posso pegar o refetch também que criar um botam e executar esta funcao para fazer um reflash
+  const { data, isLoading, isFetching, error } = useUsers();
+
   const isWideVersion = useBreakpointValue({
     base: false,
     lg: true,
@@ -30,7 +35,10 @@ export default function UserList() {
 
         <Box flex="1" borderRadius={8} bg="gray.800" p={["3", "6"]}>
           <Flex mb="8" justify="space-between" align="center">
-            <Heading size="lg" fontWeight="normal">Usuários</Heading>
+            <Heading size="lg" fontWeight="normal">
+              Usuários 
+              {(!isLoading && isFetching) && <Spinner size="sm" color='gray.500' ml='4'/>}
+              </Heading>
             <Link href="/users/create" passHref>
               <Button
                 as="a"
@@ -44,61 +52,85 @@ export default function UserList() {
             </Link>
           </Flex>
 
-          <Table colorScheme="whiteAlpha">
-            <Thead>
-              <Tr>
-                <Th px={["4", "4", "6"]} color="gray.300" width="8">
-                  <Checkbox colorScheme="pink" />
-                </Th>
-                <Th>Usuário</Th>
-                {isWideVersion && (<Th>Data de cadastro</Th>)}
-                <Th width="8"></Th>
-              </Tr>
-            </Thead>
-            <Tbody>
-              <Tr>
-                <Td px={["4", "4", "6"]} color="gray.300" width="8">
-                  <Checkbox colorScheme="pink" />
-                </Td>
-                <Td>
-                  <Box>
-                    <Text fontWeight="bold">Fernando Alves</Text>
-                    <Text fontSize="sm" color="gray.300">Fernandoalvesq@gmail.com</Text>
-                  </Box>
-                </Td>
-                {isWideVersion && (<Td>04 de Abril de 2021</Td>)}
-                <Td>
-                  {
-                    isWideVersion ?
-                      (
-                        <Button
-                          as="a"
-                          size="sm"
-                          fontSize="16"
-                          colorScheme="purple"
-                          leftIcon={<Icon as={RiPencilLine} />}
-                        >
-                          Editar
-                        </Button>
-                      )
-                      :
-                      (
-                        <IconButton
-                          aria-label="Edit User"
-                          as="a"
-                          size="sm"
-                          fontSize="16"
-                          colorScheme="purple"
-                          icon={<Icon as={RiPencilLine} />}
-                        />
-                      )
-                  }
-                </Td>
-              </Tr>
-            </Tbody>
-          </Table>
+          {
+            isLoading ? (
+              <Flex justify="center">
+                <Spinner />
+              </Flex>
+            ) : error ? (
+              <Flex justify="center">
+                <Text>Falha ao obter dados dos usuários</Text>
+              </Flex>
+            ) : (
+              <>
+                <Table colorScheme="whiteAlpha">
+                  <Thead>
+                    <Tr>
+                      <Th px={["4", "4", "6"]} color="gray.300" width="8">
+                        <Checkbox colorScheme="pink" />
+                      </Th>
+                      <Th>Usuário</Th>
+                      {isWideVersion && (<Th textAlign="center">Data de cadastro</Th>)}
+                      <Th width="8"></Th>
+                    </Tr>
+                  </Thead>
+                  <Tbody>
+                    {
+                      data.map(user => {
+                        return (
+                          <Tr key={user.id}>
+                            <Td px={["4", "4", "6"]} color="gray.300" width="8">
+                              <Checkbox colorScheme="pink" />
+                            </Td>
+                            <Td>
+                              <Box>
+                                <Text fontWeight="bold">{user.name}</Text>
+                                <Text fontSize="sm" color="gray.300">{user.email}</Text>
+                              </Box>
+                            </Td>
+                            {isWideVersion && (<Td textAlign="center">{user.createdAt}</Td>)}
+                            <Td>
+                              {
+                                isWideVersion ?
+                                  (
+                                    <Button
+                                      as="a"
+                                      size="sm"
+                                      fontSize="16"
+                                      colorScheme="purple"
+                                      leftIcon={<Icon as={RiPencilLine} />}
+                                    >
+                                      Editar
+                                    </Button>
+                                  )
+                                  :
+                                  (
+                                    <IconButton
+                                      aria-label="Edit User"
+                                      as="a"
+                                      size="sm"
+                                      fontSize="16"
+                                      colorScheme="purple"
+                                      icon={<Icon as={RiPencilLine} />}
+                                    />
+                                  )
+                              }
+                            </Td>
+                          </Tr>
+                        )
+                      })
+                    }
+                  </Tbody>
+                </Table>
 
-          <Pagination />
+                <Pagination 
+                  totalCountOfRegisters={200}
+                  currentPage={5}
+                  onPageChange={() => {}}
+                />
+              </>
+            )
+          }
         </Box>
       </Flex>
     </Box>
